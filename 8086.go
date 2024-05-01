f52c99851baaf4c8c02b3a4b28c5b956f8830451
Jacob Cavazos-England <jacob.e.england@gmail.com>
2024-05-01 17:53:09 -0400

    feat: refactor to handle multiple opcodes

--- 8086.go
+++ 8086.go
@@ -20,8 +20,14 @@ func decodeOp(b byte) string {
 	panic(1)
 }
 
-var opcodes = map[uint8]string{
-	0b100010: "mov",
+type Instruction struct {
+	opcode    string
+	operation func(bytes []byte, instruction Instruction) int
+}
+
+var opcodes = map[uint8]Instruction{
+	0b100010: {"mov", rmToFromReg},
+	0b1011:   {"mov", immediateToReg},
 }
 
 var registers = map[uint8][2]string{
@@ -45,32 +51,68 @@ func main() {
 
 	check(err)
 
-	for i := 0; i < len(dat); i += 2 {
-		b1 := dat[i]
-		b2 := dat[i+1]
+	i := 0
+	for i < len(dat) {
+		var count int
+		bytes := getBytes(dat, i)
+		b1 := bytes[0]
 
-		opcode := b1 & 0b11111100 >> 2
-		d := b1 & 0b00000010 >> 1
-		w := b1 & 0b00000001
+		opcode4b := b1 & 0b11110000 >> 4
+		opcode6b := b1 & 0b11111100 >> 2
 
-		mod := b2 & 0b11000000 >> 6
-		reg := b2 & 0b00111000 >> 3
-		rm := b2 & 0b00000111
-
-		wide := int(w)
-
-		opcodeD := opcodes[opcode]
-		regD := registers[reg][wide]
-		rmD := registers[rm][wide]
-
-		if mod == 0b11 {
-			if d == 0b0 {
-				fmt.Printf("%s %s, %s\n", opcodeD, rmD, regD)
-			} else {
-				fmt.Printf("%s %s, %s\n", opcodeD, regD, rmD)
-			}
-		} else {
-			panic(3)
+		// check 4 bit opcodes
+		instruction4b, ok := opcodes[opcode4b]
+		if ok {
+			count = instruction4b.operation(bytes, instruction4b)
 		}
+
+		// check 6 bit opcodes
+		instruction6b, ok := opcodes[opcode6b]
+		if ok {
+			count = instruction6b.operation(bytes, instruction6b)
+		}
+
+		i += count
 	}
+}
+
+func getBytes(data []byte, i int) []byte {
+	end := i + 6
+	if end > len(data) {
+		end = len(data)
+	}
+	return data[i:end]
+}
+
+func rmToFromReg(bytes []byte, instruction Instruction) int {
+	b1 := bytes[0]
+	b2 := bytes[1]
+
+	d := b1 & 0b00000010 >> 1
+	w := b1 & 0b00000001
+
+	mod := b2 & 0b11000000 >> 6
+	reg := b2 & 0b00111000 >> 3
+	rm := b2 & 0b00000111
+
+	wide := int(w)
+
+	regD := registers[reg][wide]
+	rmD := registers[rm][wide]
+
+	if mod == 0b11 {
+		if d == 0b0 {
+			fmt.Printf("%s %s, %s\n", instruction.opcode, rmD, regD)
+			return 2
+		} else {
+			fmt.Printf("%s %s, %s\n", instruction.opcode, regD, rmD)
+			return 2
+		}
+	} else {
+		panic(3)
+	}
+}
+
+func immediateToReg(bytes []byte, instruction Instruction) int {
+	return 0
 }
